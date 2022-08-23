ad9448a5e6872c5b341643a671a972fc60ec63f9
ritika <ritikasinghal2101@gmail.com>
2022-08-23 20:35:18 +0530

    changes

--- src/components/Context.js
+++ src/components/Context.js
@@ -5,16 +5,7 @@ export const mycontext=createContext();
 function Appcontext({children}) {
     const [meals,setmeals]=React.useState([]);
     const [categories,setcategories]=React.useState([]);
-    const [recinfo,setrecinfo]=React.useState([]);
-
-const fetchinfo=React.useCallback((select)=>{
-  axios.get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${select}`)
-  .then(res=>{console.log(res.data.meals);
-    setrecinfo(res.data.meals)})
-
-},[]);    
-
-const fetchmeals=React.useCallback((searcht)=>{
+   const fetchmeals=React.useCallback((searcht)=>{
    axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searcht}`)
 .then(res=>{console.log(res.data.meals);
 setmeals(res.data.meals)});
@@ -32,7 +23,7 @@ const fetchcategories=React.useCallback(
 
 
   return (
-    <mycontext.Provider value={{fetchmeals,meals,fetchcategories,categories,fetchinfo,recinfo}}>{children}</mycontext.Provider>
+    <mycontext.Provider value={{fetchmeals,meals,fetchcategories,categories}}>{children}</mycontext.Provider>
   )
 }
 
